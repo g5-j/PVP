633e52a4b27892ee6b90ab71a498fe3743085732
ABOOD <abdovop0@gmail.com>
2026-08-30 17:04:39 +0200

    Format code in PVP.java for consistency

--- src/main/java/com/author/pvp/PVP.java
+++ src/main/java/com/author/pvp/PVP.java
@@ -5,12 +5,12 @@ import com.author.pvp.events.AttackEventHandler;
 import com.author.pvp.config.WTapConfig;
 
 public class PVP implements ModInitializer {
-	public static final String MOD_ID = "pvp";
-	public static final String MOD_NAME = "PVP";
+    public static final String MOD_ID = "pvp";
+    public static final String MOD_NAME = "PVP";
 
-	@Override
-	public void onInitialize() {
-		WTapConfig.load();
-		AttackEventHandler.registerAttackListener();
-	}
+    @Override
+    public void onInitialize() {
+        WTapConfig.load();
+        AttackEventHandler.registerAttackListener();
+    }
 }
