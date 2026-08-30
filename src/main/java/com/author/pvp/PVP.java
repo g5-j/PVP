ddb19732190c6b59a9646a3d7084899224e54f17
ABOOD <abdovop0@gmail.com>
2026-08-30 16:41:21 +0200

    Implement PVP mod initializer with event handlers

---
+++ src/main/java/com/author/pvp/PVP.java
@@ -0,0 +1,16 @@
+package com.author.pvp;
+
+import net.fabricmc.api.ModInitializer;
+import com.author.pvp.events.AttackEventHandler;
+import com.author.pvp.config.WTapConfig;
+
+public class PVP implements ModInitializer {
+	public static final String MOD_ID = "pvp";
+	public static final String MOD_NAME = "PVP";
+
+	@Override
+	public void onInitialize() {
+		WTapConfig.load();
+		AttackEventHandler.registerAttackListener();
+	}
+}
